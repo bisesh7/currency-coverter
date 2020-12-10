616743e5c0a422a56316468fcf7c4ec861515cc9
bisesh shakya <bisesh.shakya@gmail.com>
2020-12-10 13:28:47 +0545

    Added incomplete navbar, created routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomeComponent from "./Components/HomeComponent";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={HomeComponent} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Components/HomeComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarComponent from "./NavbarComponent";
+
+const HomeComponent = () => {
+  return (
+    <div>
+      <NavbarComponent />
+    </div>
+  );
+};
+
+export default HomeComponent;
